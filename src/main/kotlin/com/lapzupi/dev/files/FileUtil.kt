a798481887b8eced52f650b2fe56c307678f5990
Omer Oreg <omeroreg@gmail.com>
2023-06-09 18:18:11 +0300

    Update FileUtil.kt

--- src/main/kotlin/com/lapzupi/dev/files/FileUtil.kt
+++ src/main/kotlin/com/lapzupi/dev/files/FileUtil.kt
@@ -10,41 +10,38 @@ import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 
 
-class FileUtil private constructor() {
-    init {
-        throw UnsupportedOperationException()
-    }
-    
-    companion object {
-        /**
-         * plugin.getClass().getProtectionDomain().getCodeSource()
-         */
-        @Throws(IOException::class)
-        fun getFileNamesInJar(source: CodeSource, fileCondition: Predicate<ZipEntry?>): List<String> {
-            val fileNames: MutableList<String> = ArrayList()
-            val jar = source.location
-            
-            ZipInputStream(jar.openStream()).use { zip ->
-                while (true) {
-                    val entry = zip.nextEntry ?: break
-                    if (fileCondition.test(entry)) {
-                        fileNames.add(entry.name)
-                    }
+object FileUtil {
+
+
+    /**
+     * plugin.getClass().getProtectionDomain().getCodeSource()
+     */
+    @Throws(IOException::class)
+    fun getFileNamesInJar(source: CodeSource, fileCondition: Predicate<ZipEntry?>): List<String> {
+        val fileNames: MutableList<String> = ArrayList()
+        val jar = source.location
+
+        ZipInputStream(jar.openStream()).use { zip ->
+            while (true) {
+                val entry = zip.nextEntry ?: break
+                if (fileCondition.test(entry)) {
+                    fileNames.add(entry.name)
                 }
             }
-            
-            return fileNames
         }
-        
-        fun getFileNamesFromFolder(folder: File, pattern: FileFilter): List<String> {
-            return folder.listFiles(pattern)?.map{ file -> file.name} ?: emptyList()
-        }
-        
-        fun saveFileFromJar(plugin: JavaPlugin, resourcePath: String, fileName: String, folder: File?) {
-            val file = File(folder, fileName)
-            if (!file.exists()) {
-                plugin.saveResource(resourcePath + fileName, false)
-            }
+
+        return fileNames
+    }
+
+    fun getFileNamesFromFolder(folder: File, pattern: FileFilter): List<String> {
+        return folder.listFiles(pattern)?.map { file -> file.name } ?: emptyList()
+    }
+
+    fun saveFileFromJar(plugin: JavaPlugin, resourcePath: String, fileName: String, folder: File?) {
+        val file = File(folder, fileName)
+        if (!file.exists()) {
+            plugin.saveResource(resourcePath + fileName, false)
         }
     }
+
 }
